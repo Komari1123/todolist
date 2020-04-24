56ec87715188dff13f35b3f6578d9c916a111c53
Komari1123 <komakomari11235@gmail.com>
2020-04-24 17:04:21 +0900

    管理機能の追加

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,14 @@ from django.views.generic import ListView,DetailView,CreateView,DeleteView,Updat
 from .models import TodoModel
 from django.urls import reverse_lazy
 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
+
+
+
 # Create your views here.
 class TodoList(ListView):
     template_name = 'list.html'
@@ -29,3 +37,31 @@ class TodoUpdate(UpdateView):
     fields = ('titile','memo','priority','duedate')
     success_url = reverse_lazy('list')
 
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html', {'some':100})
+    return render(request, 'signup.html', {'some':100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
