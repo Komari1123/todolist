1764dd4386eed76a0bc1dfb2c8b897f2e78ad054
Komari1123 <komakomari11235@gmail.com>
2020-04-27 10:00:00 +0900

    wsgiの編集

--- todoproject/wsgi.py
+++ todoproject/wsgi.py
@@ -2,11 +2,11 @@ import os
 
 #from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
 
 #application = Cling(get_wsgi_application())
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
